1c14ee5341e8440f5d16e622ec543033cff08a13
MichelDucartier <m.zhang2490@gmail.com>
2026-03-31 01:55:36 +0200

    Update treesitter

--- lua/michel_ducartier/plugins/treesitter.lua
+++ lua/michel_ducartier/plugins/treesitter.lua
@@ -2,10 +2,4 @@ return {
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
     build = ':TSUpdate',
-    config = function()
-        local builtin = require('nvim-treesitter.configs')
-        builtin.setup({
-            ensure_installed = { "lua", "rust", "python", "markdown" }
-        })
-    end
 }
